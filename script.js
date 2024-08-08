955deee277e7c294133ebd34359ee3f382ade2a2
santosfabin <fsfabianosantos03@gmail.com>
2024-08-08 09:08:57 -0300

    finished carousel

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-document.getElementById('scroll-left').addEventListener('click', function() {
-    document.querySelector('.container').scrollBy({
-        left: -300, // ajuste este valor conforme necessário
-        behavior: 'smooth'
-    });
+document.getElementById("scroll-left").addEventListener("click", function () {
+	document.querySelector(".container").scrollBy({
+		left: -550, // ajuste este valor conforme necessário
+		behavior: "smooth"
+	});
 });
 
-document.getElementById('scroll-right').addEventListener('click', function() {
-    document.querySelector('.container').scrollBy({
-        left: 300, // ajuste este valor conforme necessário
-        behavior: 'smooth'
-    });
+document.getElementById("scroll-right").addEventListener("click", function () {
+	document.querySelector(".container").scrollBy({
+		left: 550, // ajuste este valor conforme necessário
+		behavior: "smooth"
+	});
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	const labels = document.querySelectorAll('label[for^="header-"]');
+
+	labels.forEach(label => {
+		label.addEventListener("click", function () {
+			const targetId = this.getAttribute("for").replace("header-", "");
+			const targetElement = document.getElementById(targetId);
+
+			if (targetElement) {
+				targetElement.scrollIntoView({behavior: "smooth"});
+			}
+		});
+	});
 });
